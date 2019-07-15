c4a3ac6e0bc6cddedf6ad74ef8c0a3061086c1ab
HJQ <v-huangjiaqian@midland.local>
2019-07-15 11:33:12 +0800

    工程初始化

---
+++ Mall/Mall/Classes/Test.swift
@@ -0,0 +1,13 @@
+//
+//  Test.swift
+//  Mall
+//
+//  Created by midland on 2019/7/15.
+//  Copyright © 2019 JQHxx. All rights reserved.
+//
+
+import UIKit
+
+class Test: NSObject {
+
+}
---
+++ Mall/Mall/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Mall
+//
+//  Created by midland on 2019/7/15.
+//  Copyright © 2019 JQHxx. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
