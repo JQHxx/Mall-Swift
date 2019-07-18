cd6bb0e2b29d76fc31a90979dd6761db55661518
HJQ <v-huangjiaqian@midland.local>
2019-07-18 18:04:55 +0800

    增加桥接文件

---
+++ Mall/Mall/Mall_bridge.h
@@ -0,0 +1,13 @@
+//
+//  Mall_bridge.h
+//  Mall
+//
+//  Created by midland on 2019/7/18.
+//  Copyright © 2019 JQHxx. All rights reserved.
+//
+
+#ifndef Mall_bridge_h
+#define Mall_bridge_h
+
+
+#endif /* Mall_bridge_h */
