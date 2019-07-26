ff51ee2f31a8742b77b400b44f6a00b76ead491b
HJQ <v-huangjiaqian@midland.local>
2019-07-26 12:21:21 +0800

    提交

---
+++ Mall/Mall/Classes/Sections/Mine/View/MineHeaderView.swift
@@ -0,0 +1,16 @@
+//
+//  MineHeaderView.swift
+//  Mall
+//
+//  Created by midland on 2019/7/26.
+//  Copyright © 2019 JQHxx. All rights reserved.
+//
+
+import UIKit
+
+class MineHeaderView: UIView {
+
+    @IBOutlet weak var avatarImageView: UIImageView!
+    @IBOutlet weak var userNameLabel: UILabel!
+    
+}
